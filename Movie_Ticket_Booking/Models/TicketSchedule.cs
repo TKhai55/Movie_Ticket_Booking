c774d134f2f029e0ead3c06eeafdd5e5822a5708
TKhai55 <thienkhaiak5555@gmail.com>
2023-12-13 10:01:29 +0700

    fix new modal for schedule

---
+++ Movie_Ticket_Booking/Models/TicketSchedule.cs
@@ -0,0 +1,18 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+
+namespace Movie_Ticket_Booking.Models
+{
+    public class TicketSchedule
+    {
+        [BsonId, BsonElement("_id"), BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string schedule { get; set; } = null!;
+        public Seat seat { get; set; } = null!;
+        public Voucher? voucher { get; set; } = null!;
+        public DateTime createdAt { get; set; }
+        public DateTime updatedAt { get; set; }
+        public int price { get; set; } = 0!;
+    }
+}
